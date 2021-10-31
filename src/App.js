bf24c45139233df23dea243baae716889cbe84a8
Mahmoudicc95 <mahmoud-icc@hotmail.com>
2021-10-31 16:16:25 +0800

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import  ProfileImage  from './img/2222.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Adev-Ventures</h1>
+     <h2>Author:Mahmoud Kalb Alloz</h2>
+     <img src={ProfileImage} alt="profile-image"/>
+    </div>
+  );
+}
+
+export default App;
